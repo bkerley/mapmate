cc510f1fd5df9caafd6edc640cd7c2733caacb71
Bryce Kerley <bkerley@brycekerley.net>
2011-03-11 16:28:42 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapmate::Application.config.secret_token = '7dc3fd05c77271e23b012d5ba2cf04fc404d9ca58ee5199d82ee5e67660980fddd8a903dbadcfd45a8ef07f487d4827f948410d18797e8edb120b1a49edce59c'
