7b1cfc14a80389ff85a30e0749f01fd23b1ca0ae
Bryce Kerley <bkerley@brycekerley.net>
2011-03-11 23:53:23 -0500

    first pass at scanner model

---
+++ app/models/scan.rb
@@ -0,0 +1,30 @@
+class Scan
+  @queue = 'scanner'
+
+  def self.perform(target_id)
+    self.new(target_id).run
+  end
+
+  def initialize(target_id)
+    @target = Target.find target_id
+    @address = @target.address
+  end
+
+  def run
+    scan
+    save
+  end
+
+  def scan
+    Open3.popen3('nmap', '-iL', '-') do |i,o,e,t|
+      i.puts @target
+      i.close
+      @report = o.read
+    end
+  end
+
+  def save
+    @target.report = @report
+    @target.save
+  end
+end
