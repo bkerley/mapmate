ccfb48aa3e5a3f847f3cd30ebd54567b1ea81a8f
Bryce Kerley <bkerley@brycekerley.net>
2011-03-11 23:58:26 -0500

    enqueue callback

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,3 +1,9 @@
 class Target < ActiveRecord::Base
   validates_presence_of :address
+  after_create :add_scanner_to_queue
+
+  private
+  def add_scanner_to_queue
+    Resque.enqueue(Scan, self.id)
+  end
 end
